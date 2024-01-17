2efc2bad83f7d9e8d4bdb08d413a7127d00f303d
mvaldes <mvaldes.revilla@gmail.com>
2024-01-16 22:16:15 -0600

    removing fzf-tmux not working

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -10,7 +10,6 @@
       tmuxPlugins.vim-tmux-navigator
       tmuxPlugins.copycat
       tmuxPlugins.continuum
-      tmuxPlugins.tmux-fzf
     ];
     extraConfig = builtins.readFile ../config/tmux/tmux.conf;
   };
