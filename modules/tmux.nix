7e1b3ec9ccc8481c458004d0e10159b1ee7144df
mvaldes <mvaldes.revilla@gmail.com>
2024-01-22 12:31:39 -0600

    tmux plugin overrides

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -11,7 +11,9 @@
       tmuxPlugins.copycat
       tmuxPlugins.continuum
     ];
-    extraConfig = builtins.readFile ../config/tmux/tmux.conf;
+    extraConfig = ''
+      ${builtins.readFile ../config/tmux/tmux.conf}
+      run-shell ${pkgs.tmuxPlugins.mode-indicator}/share/tmux-plugins/mode-indicator/mode_indicator.tmux
+    '';
   };
-
 } 
