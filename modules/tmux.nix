ceee55ec04cf1de6765dacca221c4c17a614dd47
mvaldes <mvaldes.revilla@gmail.com>
2024-09-01 23:00:14 -0500

    tokyo tmux

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -11,6 +11,17 @@ let
         sha256 = "sha256-TO5OG7lqcN2sKRtdF7DgFeZ2wx9O1FVh1MSp+6EoYxc=";
       };
   };
+  tmux-tokyo-night = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tokyo-night-tmux";
+    version = "unstable-2021-05-14";
+    src = pkgs.fetchFromGitHub
+      {
+        owner = "janoamaral";
+        repo = "tokyo-night-tmux";
+        rev = "b45b742eb3fdc01983c21b1763594b549124d065";
+        sha256 = "sha256-k4CbfWdyk7m/T97ytxLOEMUKrkU5iJSIu3lvyT1B1jU=";
+      };
+  };
 in
 {
   programs.tmux = {
@@ -24,6 +35,7 @@ in
       tmuxPlugins.copycat
       tmuxPlugins.continuum
       tmux-sessionx
+      tmux-tokyo-night
     ];
     extraConfig = ''
       ${builtins.readFile ../config/tmux/tmux.conf}
