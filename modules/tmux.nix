141a046d4a8a7821ae1a612256a48dd1fbcd920c
mvaldes <mvaldes.revilla@gmail.com>
2024-09-11 17:18:06 -0500

    tmux theme

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -11,35 +11,33 @@ let
         sha256 = "sha256-TO5OG7lqcN2sKRtdF7DgFeZ2wx9O1FVh1MSp+6EoYxc=";
       };
   };
-  tmux-tokyo-night = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "tokyo-night-tmux";
-    version = "unstable-2021-05-14";
-    src = pkgs.fetchFromGitHub
-      {
-        owner = "janoamaral";
-        repo = "tokyo-night-tmux";
-        rev = "b45b742eb3fdc01983c21b1763594b549124d065";
-        sha256 = "sha256-k4CbfWdyk7m/T97ytxLOEMUKrkU5iJSIu3lvyT1B1jU=";
-      };
+  tokyo-night = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "tokyo-night";
+    version = "unstable-2023-01-06";
+    src = pkgs.fetchFromGitHub {
+      owner = "janoamaral";
+      repo = "tokyo-night-tmux";
+      rev = "master";
+      sha256 = "sha256-3rMYYzzSS2jaAMLjcQoKreE0oo4VWF9dZgDtABCUOtY=";
+    };
   };
 in
 {
   programs.tmux = {
     enable = true;
-    plugins = with pkgs; [
-      tmuxPlugins.sensible
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
       # tmuxPlugins.mode-indicator
-      tmuxPlugins.resurrect
-      tmuxPlugins.yank
-      tmuxPlugins.vim-tmux-navigator
-      tmuxPlugins.copycat
-      tmuxPlugins.continuum
+      resurrect
+      yank
+      vim-tmux-navigator
+      copycat
+      continuum
       tmux-sessionx
-      tmuxPlugins.catppuccin
-      tmux-tokyo-night
     ];
     # run-shell ${pkgs.tmuxPlugins.mode-indicator}/share/tmux-plugins/mode-indicator/mode_indicator.tmux
     extraConfig = ''
+      run-shell /nix/store/vc02gww5zqszldw0rmq1jh1iz4w4js1l-tmuxplugin-tokyo-night-unstable-2023-01-06/share/tmux-plugins/tokyo-night/tokyo-night.tmux
       ${builtins.readFile ../config/tmux/tmux.conf}
     '';
   };
