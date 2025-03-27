c6dd678cb6256daec98ac0188216c67d17021715
mvaldes <mvaldes.revilla@gmail.com>
2025-03-26 23:20:30 -0500

    adding mac setup

---
+++ mac/home.nix
@@ -0,0 +1,76 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  username = "mvaldes";
+  homePath = "/Users/${username}";
+in {
+  home.username = "mvaldes";
+  home.homeDirectory = "/Users/mvaldes";
+
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    # Utilities
+    neovim
+    tldr
+    htop
+    jq
+    tree
+    fd
+    ripgrep
+    kubectl
+    kubernetes-helm
+    gh
+    awscli2
+    doppler
+    ncdu
+    stern
+    glow
+    dig
+    alejandra
+
+    # languages
+    nodejs_20
+    jdk
+    cargo
+    lua
+
+    # lsp
+    marksman
+    ltex-ls
+  ];
+
+  home.file = {
+    ".ssh/config" = {
+      source = "${homePath}/git/dotfiles/.ssh/config";
+    };
+    ".local/bin" = {
+      source = "${homePath}/git/dotfiles/scripts";
+      recursive = true;
+    };
+    ".aws/config" = {
+      source = "${homePath}/git/dotfiles/.aws/config";
+    };
+    ".config/wezterm" = {
+      source = "${homePath}/git/dotfiles/.config/wezterm";
+    };
+    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${homePath}/git/dotfiles/.config/nvim";
+  };
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    PATH = "$PATH:$HOME/.local/bin";
+  };
+
+  programs.home-manager.enable = true;
+  programs.zsh.enable = true;
+
+  imports = [
+    "${homePath}/git/dotfiles-nix/modules/git.nix"
+    "${homePath}/git/dotfiles-nix/modules/zsh.nix"
+    "${homePath}/git/dotfiles-nix/modules/shell.nix"
+    "${homePath}/git/dotfiles-nix/modules/tmux.nix"
+  ];
+}
