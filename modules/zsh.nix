76ec71e840de46b2f0411efa0b88c4206242125a
mvaldes <mvaldes.revilla@gmail.com>
2025-05-07 09:25:45 -0500

    deprecated setting

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -13,7 +13,7 @@
     enableCompletion = true;
     dotDir = ".config/zsh";
     syntaxHighlighting.enable = true;
-    initExtra = ''
+    initContent = ''
       source $HOME/.config/zsh/plugins/fzf-tab/fzf-tab.plugin.zsh
       source $HOME/git/dotfiles/.config/zsh/zsh_functions
       # export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
