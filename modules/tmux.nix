c4638d6b9564a745ac2a24316fc506652f5b2eae
mvaldes <mvaldes.revilla@gmail.com>
2025-06-04 11:01:18 -0500

    tmux-thumbs

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,16 +1,4 @@
 {pkgs, ...}: let
-  tmux-sessionx = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "sessionx";
-    version = "unstable-2023-01-06";
-    src =
-      pkgs.fetchFromGitHub
-      {
-        owner = "omerxx";
-        repo = "tmux-sessionx";
-        rev = "ac9b0ec219c2e36ce6beb3f900ef852ba8888095";
-        sha256 = "sha256-TO5OG7lqcN2sKRtdF7DgFeZ2wx9O1FVh1MSp+6EoYxc=";
-      };
-  };
   isMac = pkgs.stdenv.isDarwin;
   homeDir =
     if isMac
@@ -25,6 +13,8 @@ in {
       vim-tmux-navigator
       copycat
       continuum
+      tmux-sessionx
+      tmux-thumbs
       tmux-sessionx
     ];
     extraConfig = ''
