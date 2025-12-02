849f4f66e80da569ffb3fb369c28ec7a1c3f2329
mvaldes <mvaldes.revilla@gmail.com>
2025-12-02 08:40:53 -0600

    fixing opencode path

--- mac/home.nix
+++ mac/home.nix
@@ -76,7 +76,7 @@ in {
 
   home.sessionVariables = {
     EDITOR = "nvim";
-    PATH = "$PATH:$HOME/.local/bin:$HOME/.vorpal/bin;/$HOME/.opencode/bin";
+    PATH = "$PATH:$HOME/.local/bin:$HOME/.vorpal/bin;$HOME/.opencode/bin";
     LANG = "en_US.UTF-8";
   };
 
